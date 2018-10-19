cd3c31862aa507882b026aff513220b07b29d67f
nas <nash18xy@gmail.com>
2018-10-19 11:04:14 +0800

    update pada layout dan function utk display

--- app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
+++ app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
@@ -4,9 +4,13 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -15,7 +19,9 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         LayoutInflater inflater;
 
         View rowView;
-        View textValue;
+        View textValue = null;
         int X=0;
 
         parentLinearLayout = outerLinearLayout = (LinearLayout)  findViewById(OuterLayer);
@@ -55,6 +61,7 @@ public class MainActivity extends AppCompatActivity {
                 String tab = row.getString("tab");
                 String type = row.getString("type");
                 JSONArray elements = row.getJSONArray("element");
+
                 if(type.equals("0")){
                     rowView = inflater.inflate(R.layout.nb_row_group_000,null);
                     outerLinearLayout.addView(rowView,outerLinearLayout.getChildCount());
@@ -63,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
 
                 X = getLayout(type);
                 rowView = inflater.inflate(X,null);
+                List<String> strings = new ArrayList<>();
 
                 for(int y = 0; y < elements.length(); y++){
                     int target = R.id.e1;
@@ -70,20 +78,40 @@ public class MainActivity extends AppCompatActivity {
                     JSONObject ele = elements.getJSONObject(y);
                     String ee = "e" + (y + 1);
                     String label = ele.getString(ee);
+                    String alignment = ele.getString("a");
                     textValue = rowView.findViewById(target);
                     if(textValue instanceof TextView){
                         ((TextView) textValue).setText(label);
+                        switch (alignment){
+                            case "0":
+                                ((TextView) textValue).setGravity(Gravity.START);
+                                break;
+                            case "1":
+                                ((TextView) textValue).setGravity(Gravity.CENTER);
+                                break;
+                            case "2":
+                                ((TextView) textValue).setGravity(Gravity.END);
+                                break;
+                        }
                     }
-                    else{
-
-                        Log.d("logTag", "instanceof untuk = " + label);
+                    else if (type.equals("5") && y>0){
+                        strings.add(label);
+                        textValue = rowView.findViewById(R.id.e2);
                     }
-
+                    else if (textValue instanceof ImageView) {
+                        ((ImageView)textValue).setImageResource(R.drawable.gf);
+                        ((ImageView)textValue).setScaleType(ImageView.ScaleType.FIT_XY);
+                    }
+                }
+                if (textValue instanceof Spinner) {
+                    ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, strings);
+                    dataAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
+                    ((Spinner) textValue).setAdapter(dataAdapter);
                 }
                 parentLinearLayout.addView(rowView,parentLinearLayout.getChildCount());
             }
         } catch (JSONException e) {
-            Log.d("tagLog", "Search : " + e.toString());
+            Log.d("logTag", "Search : " + e.toString());
         }
     }
 
