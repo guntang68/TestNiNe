9366389119a2a8418fa7a0e017a76ee3fec14b79
AZAbdullah <annuarzainiabdullah68@gmail.com>
2018-10-20 19:42:33 +0800

    Process JSON

--- app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
+++ app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
@@ -1,38 +1,21 @@
 package com.a9be.annuar.testnine;
 
-import android.content.Context;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.Transformation;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.ScrollView;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private boolean mula;
+
+    Test test;
 
     @Override
     public void onClick(View v) {
@@ -131,115 +114,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    private int getLayout(String type)
-    {
-        int X = R.layout.row_layout_000;
-        try {
-            X += Integer.parseInt(type);
-        } catch(NumberFormatException nfe) {
-            Log.d("logTag","error file");
-            System.out.println("Could not parse " + nfe);
-        }
-        return X;
-    }
-
-    private void bukaJson(int OuterLayer)
-    {
-        LinearLayout outerLinearLayout;
-        LinearLayout parentLinearLayout;
-        LayoutInflater inflater;
-
-        View rowView;
-        View textValue = null;
-        int X=0;
-
-        parentLinearLayout = outerLinearLayout = (LinearLayout)  findViewById(OuterLayer);
-        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        String json = loadJSONFromAsset();
-        try {
-            JSONObject main = new JSONObject(json);
-            setTitle(main.get("n").toString());
-            JSONArray rows = main.getJSONArray("rows");
-
-            for(int i=0; i<rows.length(); i++){
-                JSONObject row = rows.getJSONObject(i);
-                String tab = row.getString("tab");
-                String type = row.getString("type");
-                JSONArray elements = row.getJSONArray("element");
-
-                if(mula || type.equals("0")){
-                    if (mula)
-                        mula = false;
-                    parentLinearLayout  = (LinearLayout) inflater.inflate(R.layout.nb_row_group_000,null);
-                    outerLinearLayout.addView(parentLinearLayout,outerLinearLayout.getChildCount());
-                }
-
-                X = getLayout(type);
-                rowView = inflater.inflate(X,null);
-
-//// NAS
-                if (type.equals("0"))
-                    rowView.setOnClickListener(this);
-                List<String> strings = new ArrayList<>();
-
-/// NAS habis
-
-                for(int y = 0; y < elements.length(); y++){
-                    int target = R.id.e1;
-                    target += y ;
-                    JSONObject ele = elements.getJSONObject(y);
-                    String ee = "e" + (y + 1);
-                    String label = ele.getString(ee);
-                    textValue = rowView.findViewById(target);
-
-//// Nas start
-                    String alignment = ele.getString("a");
-                    if(textValue instanceof TextView){
-                        ((TextView) textValue).setText(label);
-                        switch (alignment){
-                            case "0":
-                                ((TextView) textValue).setGravity(Gravity.START);
-                                break;
-                            case "1":
-                                ((TextView) textValue).setGravity(Gravity.CENTER);
-                                break;
-                            case "2":
-                                ((TextView) textValue).setGravity(Gravity.END);
-                                break;
-                        }
-                    }
-                    else if (type.equals("5") && y>0){
-                        strings.add(label);
-                        textValue = rowView.findViewById(R.id.e2);
-                    }
-                    else if (textValue instanceof ImageView) {
-                        ((ImageView)textValue).setImageResource(R.drawable.gf);
-                        ((ImageView)textValue).setScaleType(ImageView.ScaleType.FIT_XY);
-                    }
-                }
-                if (textValue instanceof Spinner) {
-                    ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this, R.layout.spinner_item, strings);
-                    dataAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item);
-                    ((Spinner) textValue).setAdapter(dataAdapter);
-                }
-
-/// NAS habis
-                parentLinearLayout.addView(rowView,parentLinearLayout.getChildCount());
-            }
-        } catch (JSONException e) {
-            Log.d("logTag", "Search : " + e.toString());
-        }
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mula = true;
+        test = new Test();
 
-        bukaJson(R.id.outer_layout);
+//        bukaJson(R.id.outer_layout);
+
+        test.bukaJson(R.id.outer_layout,this,this);
     }
 
     public void onEdit(View v){
@@ -247,23 +132,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
 
-    public String loadJSONFromAsset() {
-        String json;
-        try {
-            InputStream is = getApplicationContext().getAssets().
-                    open("baseData.json");
-            int size = is.available();
-            byte[] buffer = new byte[size];
-            is.read(buffer);
-            is.close();
-            json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            return null;
-        }
-        Log.d("json", "dpt baca");
-        return json;
-    }
+
 
 
 }
+
+
+
+
+
+
