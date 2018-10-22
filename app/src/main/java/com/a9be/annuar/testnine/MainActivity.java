2e8a74674d75a30b120eb48ae33a20f5e1b5d4ea
AZAbdullah <annuarzainiabdullah68@gmail.com>
2018-10-22 10:30:28 +0800

    Process JSON

--- app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
+++ app/src/main/java/com/a9be/annuar/testnine/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends AppCompatActivity{
 
         test.editText(R.id.outer_layout,this,0,1,1,"Annuar Zaini Abdullah");
         test.editText(R.id.outer_layout,this,0,2,0,"A quick brown fox jumps over the lazy dog");
+        test.editText(R.id.outer_layout,this,1,2,0,"Timestamp");
 
     }
 
